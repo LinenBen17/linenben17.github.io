9a4454e58f25417b8e559cc9955d77fb40721f65
LinenBen17 <91392249+LinenBen17@users.noreply.github.com>
2022-05-31 14:42:16 -0600

    Add files via upload

---
+++ countdown/index.js
@@ -0,0 +1,19 @@
+$(".example").TimeCircles({ time: {
+    Days: { 
+       	text: "Días",
+       	color: "#620396" 
+    },
+    Hours: { 
+       	text: "Horas",
+       	color: "#7803B8" 
+    },
+    Minutes: { 
+       	text: "Minutos",
+       	color: "#9811E2" 
+    },
+    Seconds: { 
+       	text: "Segundos",
+       	color: "#A334E0" 
+    }
+}});
+$(".example").TimeCircles({circle_bg_color:"transparent"});
